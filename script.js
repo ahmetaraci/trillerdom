944c80274de79d9fd52317c276e7ed81b3b1d19a
Muhammet Ahmet ARACI <muhammetahmet.araci@gmail.com>
2024-04-27 17:22:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var map = L.map("map").setView([39.925533, 32.866287], 6);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
